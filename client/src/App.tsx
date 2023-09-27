42f674468ef11cebb5f8bd2d39811cbbc25865f8
Tahlil <bsse0803@iit.du.ac.bd>
2023-09-27 21:10:18 +0600

    Improve ui for adding task

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,23 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
 import logo from "./logo.svg";
 import { Provider, Network } from "aptos";
 import "./App.css";
 import { WalletSelector } from "@aptos-labs/wallet-adapter-ant-design";
-import { Layout, Row, Col, Button } from "antd";
+import { Layout, Row, Col, Button, Spin } from "antd";
 import "@aptos-labs/wallet-adapter-ant-design/dist/index.css";
 import { useWallet } from "@aptos-labs/wallet-adapter-react";
 const provider = new Provider(Network.DEVNET);
 
 function App() {
+  const moduleAddress =
+  "0xaeb489f1b55ebb7b00926879ea2dceaf7bac94556649b83e1dd1a2bf41c017c8";
   const [accountHasList, setAccountHasList] = useState<boolean>(false);
+  const [transactionInProgress, setTransactionInProgress] = useState<boolean>(false);
   const fetchList = async () => {
     if (!account) return [];
     // change this to be your module account address
-    const moduleAddress = "0xaeb489f1b55ebb7b00926879ea2dceaf7bac94556649b83e1dd1a2bf41c017c8";
+
     try {
       const TodoListResource = await provider.getAccountResource(
         account.address,
@@ -25,35 +28,76 @@ function App() {
       setAccountHasList(false);
     }
   };
-  const { account } = useWallet();
+
+  const addNewList = async () => {
+    if (!account) return [];
+    setTransactionInProgress(true);
+    // build a transaction payload to be submited
+    const payload = {
+      type: "entry_function_payload",
+      function: `${moduleAddress}::todolist::create_list`,
+      type_arguments: [],
+      arguments: [],
+    };
+    try {
+      // sign and submit transaction to chain
+      const response = await signAndSubmitTransaction(payload);
+      // wait for transaction
+      await provider.waitForTransaction(response.hash);
+      setAccountHasList(true);
+    } catch (error: any) {
+      setAccountHasList(false);
+    } finally {
+      setTransactionInProgress(false);
+    }
+  };
+  
+
+  
+  const { account, signAndSubmitTransaction } = useWallet();
   useEffect(() => {
     fetchList();
   }, [account?.address]);
   return (
     <>
-    <Layout>
-      <Row align="middle">
-        <Col span={10} offset={2}>
-          <h1>Our todolist</h1>
-        </Col>
-        <Col span={12} style={{ textAlign: "right", paddingRight: "200px" }}>
-          <WalletSelector />
-        </Col>
-      </Row>
-    </Layout>
-    {!accountHasList && (
-      <Row gutter={[0, 32]} style={{ marginTop: "2rem" }}>
-        <Col span={8} offset={8}>
-          <Button block type="primary" style={{ height: "40px", backgroundColor: "#3f67ff" }}>
-            Add new list
-          </Button>
-        </Col>
-      </Row>
-    )}
-  </>
+      <Layout>
+        <Row align="middle">
+          <Col span={10} offset={2}>
+            <h1>Our todolist</h1>
+          </Col>
+          <Col span={12} style={{ textAlign: "right", paddingRight: "200px" }}>
+            <WalletSelector />
+          </Col>
+        </Row>
+      </Layout>
+      {!accountHasList && (
+        <Row gutter={[0, 32]} style={{ marginTop: "2rem" }}>
+          <Col span={8} offset={8}>
+            <Button
+              onClick={addNewList}
+              block
+              type="primary"
+              style={{ height: "40px", backgroundColor: "#3f67ff" }}
+            >
+              Add new list
+            </Button>
+          </Col>
+        </Row>
+      )}
+      <Spin spinning={transactionInProgress}>
+      {!accountHasList && (
+        <Row gutter={[0, 32]} style={{ marginTop: "2rem" }}>
+          <Col span={8} offset={8}>
+            <Button onClick={addNewList} block type="primary" style={{ height: "40px", backgroundColor: "#3f67ff" }}>
+              Add new list
+            </Button>
+          </Col>
+        </Row>
+      )}
+    </Spin>
+    </>
   );
 }
 
 export default App;
-
 
